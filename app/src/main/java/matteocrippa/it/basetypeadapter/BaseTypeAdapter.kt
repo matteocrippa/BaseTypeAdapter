27a355ac55a66ce56f01bb2413a579e67021c6df
Matteo Crippa <matteo@boostco.de>
2017-11-19 20:48:57 +0100

    cleaned

--- app/src/main/java/matteocrippa/it/basetypeadapter/BaseTypeAdapter.kt
+++ app/src/main/java/matteocrippa/it/basetypeadapter/BaseTypeAdapter.kt
@@ -3,5 +3,23 @@ package matteocrippa.it.basetypeadapter
 /**
  * Created by matteocrippa on 19/11/2017.
  */
-class BaseTypeAdapter {
+import android.widget.BaseAdapter
+import kotlin.properties.Delegates
+
+abstract class BaseTypeAdapter<T> : BaseAdapter() {
+    var data: List<T> by Delegates.observable(listOf()) { _, _, _ ->
+        this.notifyDataSetChanged()
+    }
+
+    override fun getCount(): Int {
+        return data.count()
+    }
+
+    override fun getItem(p0: Int): Any {
+        return data[p0] as Any
+    }
+
+    override fun getItemId(p0: Int): Long {
+        return p0.toLong()
+    }
 }
